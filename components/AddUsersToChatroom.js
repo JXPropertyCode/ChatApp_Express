caaaace1d653a82d9c7539f02fbaeee4de6dd35e
JXPropertyCode <yellowdice92@hotmail.com>
2021-11-10 15:17:31 -0800

    solved async for loop accountFind

--- components/AddUsersToChatroom.js
+++ components/AddUsersToChatroom.js
@@ -1,69 +1,68 @@
 const Account = require("../models/AccountObject");
 
+async function filterMembers(reqData) {
+	let memberStatus = {
+		addedMembers: [],
+		notAddedMembers: [],
+	};
+	for (let i = 0; i < reqData.addMembersList.length; i++) {
+		// using callbacks such as Account.find({}, () => {}). is already async. And would result in call back hell in this senario
+		let isUserFound = await Account.find({ _id: reqData.addMembersList[i] })
+			.then((data) => (data ? true : false))
+			.catch((err) => false);
+		if (isUserFound) {
+			memberStatus.addedMembers.push(reqData.addMembersList[i]);
+		} else {
+			memberStatus.notAddedMembers.push(reqData.addMembersList[i]);
+		}
+	}
+	console.log("After memberStatus:", memberStatus);
+	return memberStatus;
+}
+
 const AddUsersToChatroom = (req, res) => {
 	console.log("Inside Add Users to Chatroom...");
-	// 	let reqData = req.body;
+	let reqData = req.body;
 
-	// 	console.log("reqData:", reqData);
+	console.log("reqData:", reqData);
 
-	// 	// finding the userId to add the user to the chatroom
-
-	// 	let memberStatus = {
-	// 		addedMembers: [],
-	// 		notAddedMembers: [],
-	// 	};
-
-	// 	async function filterMembers() {
-	// 		await reqData.addMembersList.forEach( async (members) => {
-	// 			console.log("Members:", members);
-	// 			try {
-	// 				Account.find({ _id: members }, function (err, data) {
-	// 					if (err) {
-	// 						console.log(err);
-	// 						res.send({ errorMessage: "This isn't a valid input." });
-	// 						return err;
-	// 					}
-
-	// 					// console.log("data[0]:", data[0]);
-
-	// 					if (data.length === 1) {
-	// 						// res.send({
-	// 						// 	validCred: "true",
-	// 						// });
-	// 						console.log("Account Found:", data[0]);
-
-	// 						memberStatus.addedMembers = [
-	// 							...memberStatus.addedMembers,
-	// 							members,
-	// 						];
-	// 						console.log("memberStatus:", memberStatus);
-	// 					} else {
-	// 						// res.send({ validCred: "false" });
-	// 						console.log("Account NOT Found:");
-	// 						memberStatus.notAddedMembers = [
-	// 							...memberStatus.notAddedMembers,
-	// 							members,
-	// 						];
-	// 						console.log("memberStatus:", memberStatus);
-	// 					}
-	// 					return;
-	// 				});
-	// 			} catch (error) {
-	// 				console.error(error);
-	// 			}
-	// 		});
-	// 		console.log("Before memberStatus:", memberStatus);
-	// 		return memberStatus;
-	// 	}
-
-	// 	const result = filterMembers().then((result) => {
-	// 		console.log("Final memberStatus:", result);
-
-	// 		return result;
-	// 	});
-
-	// 	// console.log("Final memberStatus:", filterMembers().then((result) => result));
-	// 	// return memberStatus;
+	// finding the userId to add the user to the chatroom
+	filterMembers(reqData);
 };
 
 module.exports = AddUsersToChatroom;
+
+// Account.find(
+// 	{ _id: reqData.addMembersList[i] },
+// 	function (err, data) {
+// 		if (err) {
+// 			console.log(err);
+// 			res.send({ errorMessage: "This isn't a valid input." });
+// 			return err;
+// 		}
+
+// 		// console.log("data[0]:", data[0]);
+
+// 		if (data.length === 1) {
+// 			// res.send({
+// 			// 	validCred: "true",
+// 			// });
+// 			console.log("Account Found:", data[0]);
+
+// 			memberStatus.addedMembers = [
+// 				...memberStatus.addedMembers,
+// 				reqData.addMembersList[i],
+// 			];
+// 			console.log("memberStatus:", memberStatus);
+// 		} else {
+// 			// res.send({ validCred: "false" });
+// 			console.log("Account NOT Found:");
+// 			memberStatus.notAddedMembers = [
+// 				...memberStatus.notAddedMembers,
+// 				reqData.addMembersList[i],
+// 			];
+// 			console.log("memberStatus:", memberStatus);
+// 		}
+// 		return;
+// 	}
+// );
