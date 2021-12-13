eb0a587635f346b01914052877f0253a90d87934
Johnny Xian <yellowdice92@hotmail.com>
2021-12-12 20:58:50 -0800

    changed EXPRESS_SERVER to REACT_APP_URL since it has to go to front end before the front end processes the backend stuff;

--- components/Email/TemplateEmail.js
+++ components/Email/TemplateEmail.js
@@ -9,31 +9,31 @@ module.exports = {
   confirm: id => ({
     subject: 'Confirm Your Account',
     html: `
-      <a href='${process.env.EXPRESS_SERVER}confirm/${id}'>
+      <a href='${process.env.REACT_APP_URL}confirm/${id}'>
         Click to Confirm Account!
       </a>
     `,      
-    text: `Copy and paste this link: ${process.env.EXPRESS_SERVER}confirm/${id}`
+    text: `Copy and paste this link: ${process.env.REACT_APP_URL}confirm/${id}`
   }),
 
   changeEmail: id => ({
     subject: 'Request to Change Your Email',
     html: `
-      <a href='${process.env.EXPRESS_SERVER}change-email/${id}'>
+      <a href='${process.env.REACT_APP_URL}change-email/${id}'>
         Click to Change Your Email!
       </a>
     `,      
-    text: `Copy and paste this link: ${process.env.EXPRESS_SERVER}change-email/${id}`
+    text: `Copy and paste this link: ${process.env.REACT_APP_URL}change-email/${id}`
   }),
 
   changePassword: id => ({
     subject: 'Request to Change Your Password',
     html: `
-      <a href='${process.env.EXPRESS_SERVER}change-password/${id}'>
+      <a href='${process.env.REACT_APP_URL}change-password/${id}'>
         Click to Change Your Email!
       </a>
     `,      
-    text: `Copy and paste this link: ${process.env.EXPRESS_SERVER}change-password/${id}`
+    text: `Copy and paste this link: ${process.env.REACT_APP_URL}change-password/${id}`
   }),
   
 }
